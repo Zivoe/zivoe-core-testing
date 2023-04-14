a369bbf28b52ff79fcd6449274e9faf95043300d
Alexandre Collette <alexcollette@hotmail.com>
2023-04-14 12:10:29 +0200

    redemptionRequests restrictions + distributeEpoch() state

--- src/TESTS_OCR/Test_OCR_Modular.sol
+++ src/TESTS_OCR/Test_OCR_Modular.sol
@@ -53,7 +53,7 @@ contract Test_OCR_Modular is Utility {
 
     }
 
-    // Validate initial state.
+    // Validate redemptionRequestJunior() state changes
     function test_OCR_redemptionRequestJunior_state() public {
 
         uint256 amountToRedeem = 2_000_000 ether;
@@ -76,7 +76,29 @@ contract Test_OCR_Modular is Utility {
         assert(OCR_Modular_DAI.userClaimTimestamp(address(jim)) == block.timestamp);
     }    
 
-    // Validate initial state.
+    // Validate redemptionRequestJunior() restrictions
+    function test_OCR_redemptionRequestJunior_restrictions() public {
+
+        uint256 amountToRedeem = 20_000_000 ether;
+        assert(OCR_Modular_DAI.withdrawRequestsNextEpoch() == 0);
+
+        // Withdraw staked tranche tokens
+        hevm.startPrank(address(jim));
+        stJTT.fullWithdraw();
+        IERC20(zJTT).safeApprove(address(OCR_Modular_DAI), amountToRedeem);
+        // initial values
+        uint256 userInitBalance = IERC20(zJTT).balanceOf(address(jim));
+        assert(userInitBalance < amountToRedeem);
+        // checks
+        hevm.expectRevert("OCR_Modular::redemptionRequestJunior() balanceOf(_msgSender()) < amount");
+        // call function
+        OCR_Modular_DAI.redemptionRequestJunior(amountToRedeem);
+
+        hevm.stopPrank();
+
+    }   
+
+    // Validate redemptionRequestSenior() state changes
     function test_OCR_redemptionRequestSenior_state() public {
 
         uint256 amountToRedeem = 10_000_000 ether;
@@ -98,6 +120,62 @@ contract Test_OCR_Modular is Utility {
         assert(OCR_Modular_DAI.seniorBalances(address(sam)) == amountToRedeem);
         assert(OCR_Modular_DAI.userClaimTimestamp(address(sam)) == block.timestamp);
     }  
+
+    // Validate redemptionRequestSenior() restrictions
+    function test_OCR_redemptionRequestSenior_restrictions() public {
+
+        uint256 amountToRedeem = 20_000_000 ether;
+        assert(OCR_Modular_DAI.withdrawRequestsNextEpoch() == 0);
+
+        // Withdraw staked tranche tokens
+        hevm.startPrank(address(sam));
+        stSTT.fullWithdraw();
+        IERC20(zSTT).safeApprove(address(OCR_Modular_DAI), amountToRedeem);
+        // initial values
+        uint256 userInitBalance = IERC20(zSTT).balanceOf(address(sam));
+        assert(userInitBalance < amountToRedeem);
+        // check
+        hevm.expectRevert("OCR_Modular::redemptionRequestSenior() balanceOf(_msgSender()) < amount");
+        // call function
+        OCR_Modular_DAI.redemptionRequestSenior(amountToRedeem);
+        hevm.stopPrank();
+
+    }  
+
+    // Validate distributeEpoch state changes
+    function test_OCR_distributeEpoch_state() public {
+        uint256 amountToDistribute= 2_000_000 ether;
+        uint256 amountToRedeem = 4_000_000 ether;
+        // push stablecoins to the locker
+        hevm.startPrank(address(DAO));
+        IERC20(DAI).safeApprove(address(OCR_Modular_DAI), amountToDistribute);
+        OCR_Modular_DAI.pushToLocker(DAI, amountToDistribute, "");
+        hevm.stopPrank();
+
+        // simulate a redemption request
+        hevm.startPrank(address(sam));
+        stSTT.fullWithdraw();
+        IERC20(zSTT).safeApprove(address(OCR_Modular_DAI), amountToRedeem);
+        OCR_Modular_DAI.redemptionRequestSenior(amountToRedeem);
+        hevm.stopPrank();
+
+        // warp time to next epoch distribution
+        hevm.warp(block.timestamp + 30 days + 1);
+
+        // pre check
+        assert(IERC20(DAI).balanceOf(address(OCR_Modular_DAI)) == amountToDistribute);
+        assert(OCR_Modular_DAI.withdrawRequestsNextEpoch() == amountToRedeem);
+        // distribute new epoch
+        OCR_Modular_DAI.distributeEpoch();
+
+        // checks
+        assertEq(OCR_Modular_DAI.amountWithdrawableInEpoch(), amountToDistribute);
+        assertEq(OCR_Modular_DAI.nextEpochDistribution(), block.timestamp + 30 days);
+        assertEq(OCR_Modular_DAI.currentEpochDistribution(), block.timestamp);
+        assertEq(OCR_Modular_DAI.withdrawRequestsEpoch(), amountToRedeem);
+        assertEq(OCR_Modular_DAI.withdrawRequestsNextEpoch(), 0);
+
+    }
 
 
 
