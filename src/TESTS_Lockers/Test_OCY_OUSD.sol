795bbdab01fbe2cee74a977fcbecd12a5c0f25d3
pseudonaut <jquarnstrom@yahoo.com>
2023-05-04 12:59:18 -0700

    OCY_OUSD setOCTYDL() & Refactor

--- src/TESTS_Lockers/Test_OCY_OUSD.sol
+++ src/TESTS_Lockers/Test_OCY_OUSD.sol
@@ -23,9 +23,10 @@ contract Test_OCY_OUSD is Utility {
 
     using SafeERC20 for IERC20;
 
-    OCY_OUSD OCY_A;
-    OCT_DAO OCT_A;
-    OCT_YDL OCT_B;
+    OCY_OUSD OUSDLocker;
+
+    OCT_DAO TreasuryDAO;
+    OCT_YDL TreasuryYDL;
 
     address OUSD_VAULT = 0xE75D77B1865Ae93c7eaa3040B038D7aA7BC02F70;
 
@@ -33,12 +34,14 @@ contract Test_OCY_OUSD is Utility {
 
         deployCore(false);
 
-        OCT_A = new OCT_DAO(address(DAO), address(GBL));
-        OCT_B = new OCT_YDL(address(DAO), address(GBL));
+        TreasuryDAO = new OCT_DAO(address(DAO), address(GBL));
+        TreasuryYDL = new OCT_YDL(address(DAO), address(GBL));
 
-        OCY_A = new OCY_OUSD(address(DAO), address(GBL), address(OCT_B));
+        OUSDLocker = new OCY_OUSD(address(DAO), address(GBL), address(TreasuryYDL));
 
     }
+    
+    event OCTYDLSetZVL(address indexed newOCT, address indexed oldOCT);
 
     event Logger(address);
 
@@ -53,45 +56,75 @@ contract Test_OCY_OUSD is Utility {
         hex"12aa3caf0000000000000000000000001136b25047e142fa3018184793aec68fbb173ce4000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000002a8e1e676ec238d8a992307b495b45b3feaa5e860000000000000000000000001136b25047e142fa3018184793aec68fbb173ce4000000000000000000000000883816205341a6ba3c32ae8dadcebdd9d59bc2c4000000000000000000000000000000000000000000000000000001d1a94a200000000000000000000000000000000000000000000001a6651513ddf098f969a00000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000014000000000000000000000000000000000000000000000000000000000000001600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018a00000000000000000000000000000000000000000000016c00013e0000f400a007e5c0d20000000000000000000000000000000000000000000000d00000b600000600a0fd53121f512087650d7bbfc3a9f10587d7778206671719d9910d6b175474e89094c44da98b954eedeac495271d0f0044a6417ed600000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001a36ddf857a09de8316760020d6bdbf782a8e1e676ec238d8a992307b495b45b3feaa5e8600a0f2fa6b662a8e1e676ec238d8a992307b495b45b3feaa5e8600000000000000000000000000000000000000000001a7aa752c51e5c450b1d70000000000000000459b9f61774d029980a06c4eca272a8e1e676ec238d8a992307b495b45b3feaa5e861111111254eeb25477b68fb85ed929f73a96058200000000000000000000000000000000000000000000cfee7c08";
         
         // fund contract with the right amount of tokens to swap.
-        deal(assetIn, address(OCT_A), amountIn);
+        deal(assetIn, address(TreasuryDAO), amountIn);
 
-        emit log_named_uint("OCT_A assetIn pre-swap balance:", IERC20(assetIn).balanceOf(address(OCT_A)));
-        emit log_named_uint("OCT_A assetOut pre-swap balance:", IERC20(assetOut).balanceOf(address(OCT_A)));
-        emit Logger(address(OCT_A));
+        emit log_named_uint("TreasuryDAO assetIn pre-swap balance:", IERC20(assetIn).balanceOf(address(TreasuryDAO)));
+        emit log_named_uint("TreasuryDAO assetOut pre-swap balance:", IERC20(assetOut).balanceOf(address(TreasuryDAO)));
+        emit Logger(address(TreasuryDAO));
         
         assert(zvl.try_updateIsKeeper(address(GBL), address(this), true));
-        OCT_A.convertAndForward(assetIn, assetOut, dataSwap);
+        TreasuryDAO.convertAndForward(assetIn, assetOut, dataSwap);
 
-        emit log_named_uint("OCT_A assetIn after-swap balance:", IERC20(assetIn).balanceOf(address(OCT_A)));
-        emit log_named_uint("OCT_A assetOut after-swap balance:", IERC20(assetOut).balanceOf(address(DAO)));
+        emit log_named_uint("TreasuryDAO assetIn after-swap balance:", IERC20(assetIn).balanceOf(address(TreasuryDAO)));
+        emit log_named_uint("TreasuryDAO assetOut after-swap balance:", IERC20(assetOut).balanceOf(address(DAO)));
 
         // assert balances after swap are correct.
-        assertEq(0, IERC20(assetIn).balanceOf(address(OCT_A)));
+        assertEq(0, IERC20(assetIn).balanceOf(address(TreasuryDAO)));
         assert(IERC20(assetOut).balanceOf(address(DAO)) > 0);
 
-        assert(zvl.try_updateIsLocker(address(GBL), address(OCY_A), true));
-        assert(god.try_push(address(DAO), address(OCY_A), OUSD, IERC20(OUSD).balanceOf(address(DAO)), ""));
+        assert(zvl.try_updateIsLocker(address(GBL), address(OUSDLocker), true));
+        assert(god.try_push(address(DAO), address(OUSDLocker), OUSD, IERC20(OUSD).balanceOf(address(DAO)), ""));
 
-        OCY_A.rebase();
+        OUSDLocker.rebase();
 
         hevm.warp(block.timestamp + 1 days);
-        emit log_named_uint("OUSD balance:", IERC20(OUSD).balanceOf(address(OCY_A)));
+        emit log_named_uint("OUSD balance:", IERC20(OUSD).balanceOf(address(OUSDLocker)));
         deal(DAI, address(OUSD_VAULT), 100_000 ether);
         deal(USDC, address(OUSD_VAULT), 100_000 * 10**6);
         deal(FRAX, address(OUSD_VAULT), 100_000 ether);
         deal(USDT, address(OUSD_VAULT), 100_000 * 10**6);
         IVault(OUSD_VAULT).rebase();
         hevm.warp(block.timestamp + 1 days);
-        emit log_named_uint("OUSD balance:", IERC20(OUSD).balanceOf(address(OCY_A)));
+        emit log_named_uint("OUSD balance:", IERC20(OUSD).balanceOf(address(OUSDLocker)));
         deal(DAI, address(OUSD_VAULT), 100_000 ether);
         deal(USDC, address(OUSD_VAULT), 100_000 * 10**6);
         deal(FRAX, address(OUSD_VAULT), 100_000 ether);
         deal(USDT, address(OUSD_VAULT), 100_000 * 10**6);
         IVault(OUSD_VAULT).rebase();
         hevm.warp(block.timestamp + 14 days);
-        emit log_named_uint("OUSD balance:", IERC20(OUSD).balanceOf(address(OCY_A)));
+        emit log_named_uint("OUSD balance:", IERC20(OUSD).balanceOf(address(OUSDLocker)));
 
-        OCY_A.forwardYield();
+        OUSDLocker.forwardYield();
+    }
+
+    // Validate setOCTYDL() state changes.
+    // Validate setOCTYDL() restrictions.
+    // This includes:
+    //   - _msgSender() must be ZVL
+
+    function test_OCY_OUSD_setOCTYDL_restrictions_msgSender() public {
+        // Can't call if _msgSender() is not ZVL.
+        hevm.startPrank(address(bob));
+        hevm.expectRevert("OCY_OUSD::setOCTYDL() _msgSender() != IZivoeGlobals_OCY_OUSD(GBL).ZVL()");
+        OUSDLocker.setOCTYDL(address(bob));
+        hevm.stopPrank();
+    }
+
+    function test_OCY_OUSD_setOCTYDL_state(address fuzzed) public {
+        
+        // Pre-state.
+        assertEq(OUSDLocker.OCT_YDL(), address(TreasuryYDL));
+
+        // setOCTYDL().
+        hevm.expectEmit(true, true, false, false, address(OUSDLocker));
+        emit OCTYDLSetZVL(address(fuzzed), address(TreasuryYDL));
+        hevm.startPrank(address(zvl));
+        OUSDLocker.setOCTYDL(address(fuzzed));
+        hevm.stopPrank();
+
+        // Post-state.
+        assertEq(OUSDLocker.OCT_YDL(), address(fuzzed));
+
     }
 
 }
