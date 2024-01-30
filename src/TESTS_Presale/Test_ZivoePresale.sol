16bb96b43bdda71043c90b8f735b25825a70876d
pseudonaut <jquarnstrom@yahoo.com>
2024-01-29 20:39:34 -0500

    Presale Scaffolding

---
+++ src/TESTS_Presale/Test_ZivoePresale.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "../Utility/Utility.sol";
+
+import "../../lib/zivoe-core-foundry/src/misc/Presale.sol";
+
+contract Test_Presale is Utility {
+
+    using FloorMath for uint256;
+
+    function setUp() public { }
+
+}
